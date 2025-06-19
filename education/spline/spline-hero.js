e4ffbc8ff43616090f9a824b85b030a2cdaecd97
Tragic <mb9257@g.rit.edu>
2025-06-19 01:24:36 -0400

    reorganization

--- education/spline/spline-hero.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', () => {
-    const hero = document.querySelector('.hero');
-    const y = window.scrollY;
-    hero.style.transform = `translateY(${y * -0.2}px)`;
-});
