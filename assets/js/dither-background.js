7d67ca3b7aa653fedf15d37e836c675874ab37d0
Tragic <mb9257@g.rit.edu>
2025-07-09 11:45:24 -0400

    optimized dither

--- assets/js/dither-background.js
+++ assets/js/dither-background.js
@@ -1,5 +1,4 @@
-// Dither Background Effect - Vanilla Three.js implementation
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.178.0/build/three.module.js';
 
 class DitherBackground {
   constructor(container, options = {}) {
